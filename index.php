6520d144260f635386490930746408228957dec5
noelmoriano <mariano260996@gmail.com>
2020-11-10 16:31:01 -0500

    update: view origin

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link src="./sass/index.css">	
+		<title>Our team</title>
+	</head>
+	<body>
+
+	<section class="container-general-team">
+
+	</section>
+	
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<a href="./briefcase.php?idUser=1">Ir a detalle de tecnico</a>
+	</body>
+</html>

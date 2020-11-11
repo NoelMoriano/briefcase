f4af0b43558d6954eb16811dd1686eb95891b0a3
noelmoriano <mariano260996@gmail.com>
2020-11-11 17:22:10 -0500

    update: desing index

--- index.php
+++ index.php
@@ -3,21 +3,54 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link src="./sass/index.css">	
+		<link rel="stylesheet" href="./assets/css/index.css" />
 		<title>Our team</title>
 	</head>
 	<body>
 
-	<section class="container-general-team">
+	<section id="container-general-team">
+        <h1>
+            TÉCNICOS
+        </h1>
+
+        <div class="item">
+            <div class="triangle">
+            </div>
+        </div>
+
+        <div class="container-boxes">
+
+            <div class="grid-items">
+
+                <div class="box-item">
+                    <img class="user" src="https://i.pinimg.com/236x/cf/54/6b/cf546b961a3103b71bf14955a759fdcc.jpg" alt="">
+                    <p class="name">Nombre Apellido</p>
+                    <a class="btn" href="">Ver màs</a>
+                  
+                  </div>
+       
+                   <div class="box-item">   
+                      <img class="user" src="https://i.pinimg.com/236x/cf/54/6b/cf546b961a3103b71bf14955a759fdcc.jpg" alt="">
+                      <p class="name">Nombre Apellido</p>
+                      <a class="btn" href="">Ver màs</a>
+                  </div>
+      
+                   <div class="box-item"> 
+                      <img class="user" src="https://i.pinimg.com/236x/cf/54/6b/cf546b961a3103b71bf14955a759fdcc.jpg" alt=""> 
+                      <p class="name">Nombre Apellido</p>
+                      <a class="btn" href="">Ver màs</a> 
+                  </div>
+      
+                   <div class="box-item">
+                      <img class="user" src="https://i.pinimg.com/236x/cf/54/6b/cf546b961a3103b71bf14955a759fdcc.jpg" alt="">
+                      <p class="name">Nombre Apellido</p>
+                      <a class="btn" href="">Ver màs</a>
+                  </div>  
+
+            </div>
+        </div>
 
 	</section>
-
-
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<a href="./briefcase.php?userId=38">Ir a detalle de tecnico</a>
+	
 	</body>
 </html>
